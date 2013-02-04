35c5511d62c035004ffad3facc713af0c28fbee5
fawad <fq@veriqual.com>
2013-02-04 13:07:32 +0500

    Update lib/sage_one/client/ledger_accounts.rb
    ledger accounts are readonly

--- lib/sage_one/client/ledger_accounts.rb
+++ lib/sage_one/client/ledger_accounts.rb
@@ -6,21 +6,10 @@ module SageOne
         get("ledger_accounts", options)
       end
 
-      def create_ledger_account(options)
-        post('ledger_accounts', :ledger_account => options)
-      end
-
       def ledger_account(id)
         get "ledger_accounts/#{id}"
       end
 
-      def update_ledger_account(id, options)
-        put("ledger_accounts/#{id}", :ledger_account => options)
-      end
-
-      def delete_ledger_account!(id)
-        delete("ledger_accounts/#{id}")
-      end
     end
   end
 end
