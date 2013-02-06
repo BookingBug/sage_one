20c5dbd524e86e06bc294531edb1dd1467113f29
fawad <fq@veriqual.com>
2013-02-05 23:17:57 -0800

    Create sales_invoice_payments.rb

---
+++ lib/sage_one/client/sales_invoice_payments.rb
@@ -0,0 +1,22 @@
+module SageOne
+  class Client
+    module SalesInvoicePayments
+
+      def sales_invoice_payments(sales_invoice_id)
+        get("sales_invoices/#{sales_invoice_id}/payments")
+      end
+
+      def create_sales_invoice_payment(sales_invoice_id, options)
+        post('sales_invoices/#{sales_invoice_id}/payments', :payment => options)
+      end
+
+      def sales_invoice_payment(sales_invoice_id, id)
+        get("sales_invoices/#{sales_invoice_id}/payments/#{id}")
+      end
+
+      def delete_sales_invoice_payment!(sales_invoice_id, id)
+        delete("sales_invoices/#{sales_invoice_id}/payments/#{id}")
+      end
+    end
+  end
+end
